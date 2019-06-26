446fede522fe5a0ce776ed058a7aac9b6c45b10f
Tristan Willis <triking.tew@hotmail.com>
2019-06-26 07:24:15 -0500

    Fixed Home buttons

--- project-1/src/main/webapp/static/HomeJS.js
+++ project-1/src/main/webapp/static/HomeJS.js
@@ -14,3 +14,19 @@ else {
 	    window.location.href = "http://localhost:8080/project-1/static/index.html";
 	}
 }
+
+document.getElementById("account").addEventListener("click", function(){
+	window.location.href = "/project-1/static/account.html";
+})
+document.getElementById("createReport").addEventListener("click", function(){
+	window.location.href = "/project-1/static/newReport.html";
+})
+document.getElementById("pendingReport").addEventListener("click", function(){
+	window.location.href = "/project-1/static/viewPending.html";
+})
+document.getElementById("pastReport").addEventListener("click", function(){
+	window.location.href = "/project-1/static/viewPast.html";
+})
+document.getElementById("subordinate").addEventListener("click", function(){
+	window.location.href = "/project-1/static/viewSubordinates.html";
+})
